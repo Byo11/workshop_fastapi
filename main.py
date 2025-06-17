2c2bb320014f6fcaae5774c997e2819c906e1bce
Eve <Jifilm>
2025-06-17 14:36:51 +0700

    first commit

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/todos")
+async def get_todos():
+    return [
+        {"id" 1, "detail": "frist todos"},
+        {"id" 2, "detail": "seconds todos"}
+    ]
+
+counter = 0 
+@app.get("/counter")
+async def  get_counter():
+    global counter
+    counter += 1
+    return {"message" : f"counter = {counter}"}
